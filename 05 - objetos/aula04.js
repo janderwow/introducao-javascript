7fb8c8615f8a4819e5f76350a9ef8e9dd8bf3440
Jander Melo <janderwow@gmail.com>
2024-02-22 10:49:13 -0300

    funções recebendo obejtos

--- 05 - objetos/aula04.js
+++ 05 - objetos/aula04.js
@@ -15,8 +15,18 @@ class Pessoa  {
 
 };
 
+function compararIdade(p1,p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`)
+    }else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`)
+    }else{
+        console.log(`${p1.nome} e ${p2.nome} têm a mesma idade`)
+    }
+}
+
 const jander = new Pessoa('Jander WoW', 33);
+const melo    = new Pessoa('Jander Melo', 33)
 
+compararIdade(jander,melo);
 
-console.log(jander)
-jander.descrever();
